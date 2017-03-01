12110f6dc96f967fdcbbd0dcaf6b8c40dc1a92f9
vandreev <vandreevwork94@gmail.com>
2017-03-01 20:18:35 +0300

    add name to gen files

--- gen/gen.js
+++ gen/gen.js
@@ -3,36 +3,44 @@ const mkdirp = require('mkdirp');
 const path = require('path');
 const colors = require('colors/safe');
 const templates_dir = path.resolve(__dirname, './files');
-const dir = getDirForFiles();
+const main_filename = getFileName();
+const dir = path.resolve(getDirForFiles(), main_filename);
 
 fs.readdir(templates_dir)
 .then(files => {
     return Promise.all(
-        files.map(filename => {
-            return fs.readFile(path.resolve(templates_dir, filename), 'utf8')
-                .then(data => ({
-                    filename,
-                    data
-                }));
-        })
+        files.map(filename => fs.readFile(path.resolve(templates_dir, filename), 'utf8')
+            .then(data => ({
+                data,
+                extension: filename.split('.').pop()
+            })))
     );
 })
 .then(files => {
     mkdirp.sync(dir);
-    console.log(colors.bold.green(`✔ Directory ${dir} is created.`))
+    console.log(colors.bold.green(`✔ Directory ${dir} is created.`));
     return Promise.all(
-        files.map(({filename, data}) => {
+        files.map(({data, extension}) => {
             return fs.writeFile(
-                path.resolve(dir, filename),
+                path.resolve(dir, `${main_filename}.${extension}`),
                 data
-            ).then(() => console.log(colors.bold.green(`✔    File ${filename} is created.`)));
+            )
         })
     );
 })
 .then(() => console.log(colors.bold.green(`✔ All files created`)))
 .catch(e => console.log(colors.bold.red(e)));
 
+function getFileName() {
+    const file_name = process.argv.slice(2)[1];
 
+    if (!file_name) {
+        console.log(colors.bold.red('Error: Get dir name, in second argument.'));
+        throw Error();
+    }
+
+    return file_name;
+}
 
 function getDirForFiles() {
     const dir = process.argv.slice(2)[0];
